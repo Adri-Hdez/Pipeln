548ebd87f964f03910b414b35c92017a2bf68809
Adrián <adrihs.dev@gmail.com>
2022-07-28 00:27:50 +0100

    Pipeline class unit testing

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,4 +1,15 @@
-from Pipeln.pipeline import Pipeline
+import sys
+sys.path.append('../')
 
-def test():
-    pass
+from Pipeln.pipeline import Pipeline
+from auxiliar_test_methods import testOne, testStr, testTwo
+import unittest
+
+# ----------------------------------- Pipeln testing units -----------------------------------
+class Testing(unittest.TestCase):
+  def pipeline_unittests(self):
+    obj = Pipeline(methods=[(True, testOne, (2,2)), (True, testTwo, 3), (True, testStr, 'epic')])
+    self.assertIsNotNone(obj)
+
+if __name__ == '__main__':
+  unittest.main()
