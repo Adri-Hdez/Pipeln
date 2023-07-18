43f17c651b6c972d9a4e2d294267f2ceb0419187
Adrián Hernández S <adrihs.dev@gmail.com>
2023-07-18 13:43:40 +0100

    Auxiliar methods used in pipeline tests

--- tests/auxiliar_test_methods.py
+++
@@ -1,9 +0,0 @@
-# Auxiliar test methods:
-def testOne(a, b):
-  return a + b
-
-def testTwo(c):
-  return c
-
-def testStr(s):
-  return s
---
+++ tests/features/auxiliar_test_methods.py
@@ -0,0 +1,9 @@
+# Auxiliar test methods:
+def add(a: int, b: int):
+  return a + b
+
+def sub(c: int, d: int):
+  return c - d
+
+def cap(s: str):
+  return s.capitalize()
